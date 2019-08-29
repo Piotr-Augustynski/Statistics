f2dc738bfd43070a5d869043d98203581af3cc4f
Piotr-Augustynski <augustynskipiotr@gmail.com>
2019-08-29 21:41:08 +0200

    refactor statistics app

--- lib/statistics/statistics_app.rb
+++ lib/statistics/statistics_app.rb
@@ -8,8 +8,22 @@ class StatisticsApp
   end
 
   def run
-    data, extension = DataLoader.new(file_path).call
-    converted_data = DataConverter.new(data, extension).call
-    StatisticsGenerator.new(converted_data).call
+    data, extension = load_data
+    converted_data = convert_data(data, extension)
+    generate_statistics(converted_data)
+  end
+
+  private
+
+  def load_data
+    DataLoader.new(file_path).call
+  end
+
+  def convert_data(data, extension)
+    DataConverter.new(data, extension).call
+  end
+
+  def generate_statistics(data)
+    StatisticsGenerator.new(data).call
   end
 end
